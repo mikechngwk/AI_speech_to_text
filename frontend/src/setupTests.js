9c4969f53f6e7b935bc07bb1a91e2df3bc89666a
Mike Chng <mikechngwk@gmail.com>
2025-03-26 12:35:47 +0800

    added comments

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,1 +1,4 @@
+// This file runs automatically before running tests in Jest
+// It sets up the testing environment for React Testing Library
+// In a React project using Jest and React Testing Library, the setupTests.js file is commonly used to configure Jest before running tests.
 import '@testing-library/jest-dom';
