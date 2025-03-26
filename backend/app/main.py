d5a90bade2ff370f4be8d159622bb10a26413941
Mike Chng <mikechngwk@gmail.com>
2025-03-26 12:17:04 +0800

    added comments for main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
-# app/main.py
 from fastapi import FastAPI
 from app.routers import health, transcribe, transcriptions, search
 from fastapi.middleware.cors import CORSMiddleware
 
+# This is to initialize FastAPI, title you can choose whatever you like~ but good to be descriptive on its own
 app = FastAPI(title="Speech to Text FastAPI_21032025")
 @app.get("/")
+
+
+# This is the rootendpoint that is part of the assignment requirement to check if service is up or not~
 def read_root():
     return {"message": "Test1!"}
 # Include routers for different endpoints
@@ -13,6 +16,7 @@ app.include_router(transcribe.router)
 app.include_router(transcriptions.router)
 app.include_router(search.router)
 
+# CORS needs to be enabled so that frontend can access our backend API
 app.add_middleware(
      CORSMiddleware,
      allow_origins=["*"],
