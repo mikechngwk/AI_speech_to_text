0ac41cb1d73a963fa72cd7c6615add8a130918e5
Mike Chng <mikechngwk@gmail.com>
2025-03-26 12:20:08 +0800

    comments added for utils

--- backend/app/utils/file_utils.py
+++ backend/app/utils/file_utils.py
@@ -1,7 +1,9 @@
-# app/utils/file_utils.py
+
 import os
 import uuid
 
+#Generates a unique filename by adding an unique identifier to the original filename.
+# #in case user upload the same file more than once
 def get_unique_filename(filename: str) -> str:
     name, ext = os.path.splitext(filename)
     unique_name = f"{name}_{uuid.uuid4().hex}{ext}"
--- backend/app/utils/whisper.py
+++ backend/app/utils/whisper.py
@@ -1,11 +1,11 @@
 import whisper
 
-# Load the whisper-tiny model
+#Load the whisper-tiny model
+#This is a relatively small project, if project is being in production we can change the size of model
 model = whisper.load_model("tiny")
 
 
 def transcribe_audio(file_path: str) -> str:
-
     result = model.transcribe(file_path)
     transcription = result.get("text", "").strip()
     return transcription
